397749b18543cb2c7a8154e8b1827c52fd11d86a
wangxh30 <wangxh30@lenovo.com>
2018-06-20 17:18:27 +0800

    ç©创建input组件

---
+++ packages/Input/src/input.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="hea-input">
+
+        <input type="text" :placeholder="placeholder">
+    </div>
+</template>
+<style lang="less" scoped="">
+    .hea__ico{
+        font-size: 18px;
+        line-height: 18px;
+    }
+
+</style>
+<script>
+    export default{
+        name: 'HeaInput',
+        props: {
+            type: {
+                type: String,
+                default: 'default'
+            },
+            value: {
+                type: String,
+                default: 'default'
+            },
+            icon: {
+                type: String,
+                default: ''
+            },
+            plain: Boolean,
+            disabled: Boolean,
+            round: Boolean
+        },
+        data () {
+            return {
+                msg: ''
+            }
+        }
+    }
+</script>
